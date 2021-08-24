2cfb93c2beb0e9c65ee64ca4c4623f0425c436a3
leeceo97 <leeceo97@naver.com>
2021-08-25 00:40:51 +0900

    (bug)fixed untracked files

--- Server/profiles/serializers.py
+++ Server/profiles/serializers.py
@@ -1,15 +1,31 @@
-from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework import serializers, permissions
 from rest_framework.serializers import ModelSerializer
 
 from profiles.models import Profile, Group
 
 
-class ProfileCreationSerializer(serializers.ModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = '__all__'
+        fields = ('user', 'user_pk', 'nickname', 'job', 'developer_level', 'designer_and_pm_level', 'image', 'mymail', 'myinfo', 'mygit', 'stacks', 'portfolio',)
 
 class GroupCreationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = '__all__'
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated:
+            if request.user.is_staff:
+                return True
+            elif request.method in permissions.SAFE_METHODS:
+                return True
+            elif hasattr(obj, 'user_pk'):
+                return obj.user_pk == request.user.id
+            elif obj.__class__ == get_user_model():
+                return obj.user_pk == request.user.id
+            return False
+        else:
+            return False
