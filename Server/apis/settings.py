61ac5c54d6557a6bec99d1bcbcd4ad10ba3d2a17
kgw7401 <kgw7401@gmail.com>
2021-08-25 22:51:55 +0900

    (fix)endpoint 수정

--- Server/apis/settings.py
+++ Server/apis/settings.py
@@ -143,12 +143,7 @@ WSGI_APPLICATION = 'apis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = MY_DATABASES
 
 
 
--- Server/apis/urls.py
+++ Server/apis/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('api/', include('allauth.urls')),
     path('api/', include('profiles.urls')),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
