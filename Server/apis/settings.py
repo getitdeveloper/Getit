4bd5426265ff5ac78739eb83ac02d8180636160c
kgw7401 <kgw7401@gmail.com>
2021-08-26 21:07:49 +0900

    (add) 질문/자유게시판, 댓글 모델 add, (fix) accounts view github, kakao except 부분 변경, api.settings database mysql->sqlite3

--- Server/apis/settings.py
+++ Server/apis/settings.py
@@ -144,7 +144,13 @@ WSGI_APPLICATION = 'apis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = MY_DATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+# MY_DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
