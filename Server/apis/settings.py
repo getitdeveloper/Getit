5ea84ebce63ec40d53166db998b69c118d8761cd
leeceo97 <leeceo97@naver.com>
2021-09-05 14:25:27 +0900

    (fix)

--- Server/apis/settings.py
+++ Server/apis/settings.py
@@ -55,7 +55,6 @@ PROJECT_APPS = [
 THIRD_PARTY_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
 
     'allauth',
