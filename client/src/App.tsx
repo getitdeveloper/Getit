dc5a5e115bdf95eaf1ee2d247021a05fcd8da6a1
지수 박 <alyssapark96@gmail.com>
2021-09-09 11:39:27 +0900

    FIX: Scroll location on top when move page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Components/Routes';
+import ScrollToTop from './Components/ScrollToTop';
 
 function App(): JSX.Element {
   // const message = useSelector((state: RootStateOrAny) => state.user.id.message);
@@ -8,6 +9,7 @@ function App(): JSX.Element {
   return (
     <Router>
       {/* 페이지 라우팅 */}
+      <ScrollToTop />
       <Routes />
     </Router>
   );
