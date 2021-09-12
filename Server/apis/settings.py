8d08ce85aa496c7aa8a1067ced17ddab606a060e
leeceo97 <leeceo97@naver.com>
2021-09-12 09:50:18 +0900

    (modify)명세서

--- Server/apis/settings.py
+++ Server/apis/settings.py
@@ -156,13 +156,13 @@ DATABASES = {
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+# #         'ENGINE': 'django.db.backends.sqlite3',
+# #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# #     }
+# # }
+#
+# # Password validation
+# # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- Server/boards/models.py
+++ Server/boards/models.py
@@ -6,9 +6,15 @@ class CommonBoard(models.Model):
         ('free', 'free'),
         ('question', 'question'),
     )
+    CHOICES_FILTER = (
+        ('개발자','개발자'),
+        ('기획자','기획자'),
+        ('디자이너','디자이너'),
+    )
     title = models.CharField(max_length=500)
     content = models.TextField()
-    category = models.CharField(choices=CHOICES_CATEGORY, max_length=100)
+    category = models.CharField(choices=CHOICES_CATEGORY, max_length=20)
+    worker = models.CharField(choices=CHOICES_FILTER, max_length=20)
     image = models.ImageField(upload_to='board', blank=True, null=True)
     create_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
@@ -18,10 +24,17 @@ class CommonBoard(models.Model):
         ordering = ['-create_at']
 
 class RecruitmentBoard(models.Model):
+    CHOICES_FILTER = (
+        ('개발자', '개발자'),
+        ('기획자', '기획자'),
+        ('디자이너', '디자이너'),
+    )
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
-    study = models.ForeignKey('profiles.TeamProfile', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    study = models.ForeignKey('profiles.TeamProfile', on_delete=models.CASCADE, null=False, blank=False)
     developer = models.PositiveIntegerField(default=0)
     designer = models.PositiveIntegerField(default=0)
+    worker = models.CharField(choices=CHOICES_FILTER, max_length=20)
     pm = models.PositiveIntegerField(default=0)
     content = models.TextField()
     start_date = models.DateField()
