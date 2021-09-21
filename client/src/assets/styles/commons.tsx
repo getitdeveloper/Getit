5bebdc388e2e0ed046e7d46f8f7cc36f8d1546fb
지수 박 <alyssapark96@gmail.com>
2021-09-21 23:21:27 +0900

    FIX: horizontalline style to dashed line

--- client/src/assets/styles/commons.tsx
+++ client/src/assets/styles/commons.tsx
@@ -6,6 +6,6 @@ interface Size {
 
 export const HorizontalLine = styled.div`
   width: ${(props: Size) => props.width};
-  border-top: 0.01rem solid #d2d2d2;
+  border-top: 0.01rem dashed #d2d2d2;
   border-style: none none line;
 `;
