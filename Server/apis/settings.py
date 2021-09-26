51744ec2666834f80dd445e9f442d7a08c52f0f3
leeceo97 <leeceo97@naver.com>
2021-09-27 03:18:08 +0900

    (fix)default_authentication_classes

--- Server/apis/settings.py
+++ Server/apis/settings.py
@@ -83,8 +83,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        # 'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-        # 'rest_framework.authentication.TokenAuthentication'
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     ]
 }
 
