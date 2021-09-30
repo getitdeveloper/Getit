022352e1c3646449472edf58311c8357928eb600
leeceo97 <leeceo97@naver.com>
2021-09-30 23:32:30 +0900

    (fix)likes ordering

--- Server/likes/models.py
+++ Server/likes/models.py
@@ -7,11 +7,11 @@ class CommonBoardLike(models.Model):
     commonpost = models.ForeignKey('boards.CommonBoard', on_delete=models.CASCADE, blank=True, null=True, related_name='commonlikes')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     class Meta:
-        ordering = ['-commonpost']
+        ordering = ['commonpost']
 
 class RecruitBoardLike(models.Model):
     recruitpost = models.ForeignKey('boards.RecruitmentBoard', on_delete=models.CASCADE, blank=True, null=True, related_name='recruitlikes')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ['-recruitpost']
+        ordering = ['recruitpost']
