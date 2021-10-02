ef7cce751d8723f44d7a056c232d430b54e219cf
leeceo97 <leeceo97@naver.com>
2021-10-02 23:50:26 +0900

    (test)

--- Server/profiles/permissions.py
+++ Server/profiles/permissions.py
@@ -1,11 +1,11 @@
 from rest_framework import permissions
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from profiles.models import TeamProfile
 from django.contrib.auth import get_user_model
+
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
+        print(request.data.get('user'))
         if request.method in permissions.SAFE_METHODS:
             return True
         elif request.method in ('POST', 'PUT'):
@@ -16,35 +16,14 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return False
 
     def has_object_permission(self, request, view, obj):
+        print(request.data.get('user'))
         if request.user.is_authenticated:
             if request.user.is_staff:
                 return True
-            elif hasattr(obj, 'user'):
-                return obj.user.id == request.user.id
-            elif obj.__class__ == get_user_model():
-                return obj.user.id == request.user.id
-            return False
-        else:
-            return False
-
-class RecruitmentIsOwnerOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        elif request.method in ('POST', 'PUT'):
-            study_user = TeamProfile.objects.get(id=request.data.get('study')).user.id
-            return request.user.id == request.data.get('user') and request.user.id == study_user
-        elif request.method == 'DELETE':
-            return True
-        else:
-            return False
-
-
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated:
-            if request.user.is_staff:
+            elif request.method in permissions.SAFE_METHODS:
                 return True
+            elif request.method in ('POST', 'PUT'):
+                return request.user.id == request.data.get('user')
             elif hasattr(obj, 'user'):
                 return obj.user.id == request.user.id
             elif obj.__class__ == get_user_model():
