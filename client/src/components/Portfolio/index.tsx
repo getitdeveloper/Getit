deb86e9a218c4d6b08c8ef43bee11157bbb7d538
지수 박 <alyssapark96@gmail.com>
2021-10-03 00:35:34 +0900

    Update: portfolio input field

--- client/src/components/Portfolio/index.tsx
+++ client/src/components/Portfolio/index.tsx
@@ -1,5 +1,21 @@
 import * as React from 'react';
-import { PortfolioContent } from './styles';
+import { useState } from 'react';
+import { Dialog } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import imageIcon from '@assets/icons/imageIcon.svg';
+import plusIcon from '@assets/icons/plusIcon.svg';
+import { IconButton } from '@assets/styles/commons';
+import {
+  useStyles,
+  PortfolioContent,
+  PortfolioWrapper,
+  PortfolioImage,
+  PortfolioUrl,
+  AddButton,
+  PortfolioFieldWrapper,
+  SubmitPortfolioButton,
+  PortfolioInput,
+} from './styles';
 
 const portfolioDummyData = [
   {
@@ -23,12 +39,94 @@ const portfolioDummyData = [
 ];
 
 function Portfolio(): JSX.Element {
+  const classes = useStyles();
+  const [portfolios, setPortfolios] = useState(portfolioDummyData);
+  const [projectTitle, setProjectTitle] = useState('');
+  const [projectUrl, setProjectUrl] = useState('');
+  const [projectDetail, setProjectDetail] = useState('');
+  const [open, setOpen] = useState(false);
+  const AddPortfolio = () => setOpen(true);
+  const onClose = () => setOpen(false);
+
+  const onChange = (e: any) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'title':
+        setProjectTitle(value);
+        break;
+      case 'url':
+        setProjectUrl(value);
+        break;
+      case 'detail':
+        setProjectDetail(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const onAddNewPortfolio = () => {
+    const NewPortfolio = {
+      title: projectTitle,
+      url: projectUrl,
+      image: '',
+      detail: projectDetail,
+    };
+    setPortfolios([...portfolios, NewPortfolio]);
+    setOpen(false);
+  };
+
   return (
-    <div>
-      {portfolioDummyData.map((content) => (
-        <PortfolioContent key={content.title}>{content.title}</PortfolioContent>
+    <PortfolioWrapper>
+      {portfolios.map((content) => (
+        <PortfolioContent key={content.title}>
+          <PortfolioImage src={imageIcon} alt='default icon' />
+          <p>{content.title}</p>
+          <PortfolioUrl href={content.url} target='_self'>
+            {content.url}
+          </PortfolioUrl>
+        </PortfolioContent>
       ))}
-    </div>
+      <AddButton type='button' onClick={AddPortfolio}>
+        <img src={plusIcon} alt='plus icon' />
+      </AddButton>
+
+      <Dialog
+        className={classes.dialog}
+        onClose={onClose}
+        aria-labelledby='simple-dialog-title'
+        open={open}
+      >
+        <p>포트폴리오 추가</p>
+        <PortfolioFieldWrapper>
+          <PortfolioImage src={imageIcon} alt='default icon' />
+          <div>
+            <PortfolioInput
+              name='title'
+              value={projectTitle}
+              onChange={onChange}
+              placeholder='프로젝트 제목'
+            />
+            <PortfolioInput
+              name='url'
+              value={projectUrl}
+              onChange={onChange}
+              placeholder='링크를 첨부해주세요'
+            />
+            <PortfolioInput placeholder='파일을 첨부해주세요' />
+            <PortfolioInput
+              name='detail'
+              value={projectDetail}
+              onChange={onChange}
+              placeholder='간략한 설명'
+            />
+          </div>
+        </PortfolioFieldWrapper>
+        <SubmitPortfolioButton type='button' onClick={onAddNewPortfolio}>
+          완료
+        </SubmitPortfolioButton>
+      </Dialog>
+    </PortfolioWrapper>
   );
 }
 
--- client/src/components/Portfolio/styles.tsx
+++ client/src/components/Portfolio/styles.tsx
@@ -1,19 +1,69 @@
 import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
 
-export const PortfoiloContainer = styled.div`
+export const useStyles = makeStyles(() => {
+  return {
+    dialog: {
+      display: 'flex',
+      padding: '3rem 1.5rem',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  };
+});
+
+export const PortfolioWrapper = styled.div`
   display: flex;
-  flex-direction: row;
-  align-items: center;
+  flex-wrap: wrap;
+  align-items: flex-start;
   justify-content: flex-start;
 `;
 
 export const PortfolioContent = styled.div`
-  display: inline-block;
+  margin-right: 1.5rem;
+  width: 10rem;
+`;
+
+export const PortfolioImage = styled.img`
+  width: 100%;
+  height: 100%;
+  padding: 4.5rem;
   background-color: #f5f5f5;
-  height: auto;
-  width: auto;
-  padding: 3%;
-  margin-right: 2%;
-  border-radius: 17px;
-  margin-top: 2%;
+  border-radius: 1.2rem;
+`;
+
+export const PortfolioUrl = styled.a`
+  display: block;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const AddButton = styled.button`
+  width: 10rem;
+  height: 10rem;
+  background-color: #f5f5f5;
+  border-radius: 1.2rem;
+  border: 0;
+  cursor: pointer;
+`;
+
+export const PortfolioFieldWrapper = styled.div`
+  display: flex;
+`;
+
+export const PortfolioInput = styled.input`
+  width: 100%;
+  padding: 1.5rem 1.2rem;
+  border-radius: 1rem;
+  border: solid 1px #bcbcbc;
+  background-color: #fff;
+  margin: 0.8rem 0rem;
+`;
+
+export const SubmitPortfolioButton = styled.button`
+  border: 0;
+  background-color: ${(props) => props.theme.colors.main}
+  cursor: pointer;
 `;
