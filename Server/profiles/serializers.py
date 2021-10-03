292c1b398e43501265cdc6d4cb6b00fd63419280
leeceo97 <leeceo97@naver.com>
2021-10-03 17:54:12 +0900

    (fix)teamprofile model name -> title

--- Server/profiles/serializers.py
+++ Server/profiles/serializers.py
@@ -39,7 +39,7 @@ class TeamProfileSerializer(serializers.ModelSerializer):
         fields = (
             'id',
             'user',
-            'name',
+            'title',
             'content',
             'status',
             'image',
