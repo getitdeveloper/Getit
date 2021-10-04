573e5456e804eccec9dea9b381a0ca635888e4fa
leeceo97 <leeceo97@naver.com>
2021-10-05 01:21:32 +0900

    (add)static collect

--- Server/apis/settings.py
+++ Server/apis/settings.py
@@ -202,6 +202,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
