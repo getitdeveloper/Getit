67a2286b161c893ed7b7f2f8fe4a1f80a7935e4d
seungwonleee <seungwon.code@gmail.com>
2021-10-06 16:04:07 +0900

    Fix: NavBar props types

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -1,20 +1,13 @@
 import * as React from 'react';
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import { Tab } from '@material-ui/core';
-import FreeBoardPage from '@pages/FreeBoardPage';
-import QuestionBoardPage from '@pages/QuestionBoardPage';
-import RecruitBoardPage from '@pages/RecruitBoardPage';
 import { StyledTabs, StyledLabel, NavBarWrapper } from './styles';
+import { ISelectTab } from './types';
 
-function NavBar(props: any): JSX.Element {
-  const { selectTab, setSelectTab } = props;
-  // const [selectTab, setSelectTab] = useState(0);
-
+function NavBar({ selectTab, setSelectTab }: ISelectTab): JSX.Element {
   const handleChange = useCallback(
     (event: React.ChangeEvent<{ event?: EventTarget }>, selection: number) => {
       setSelectTab(selection);
-      // 선택된 탭
-      // console.log(selection);
     },
     [],
   );
@@ -27,28 +20,23 @@ function NavBar(props: any): JSX.Element {
   );
 
   return (
-    <div>
-      {/* 페이지 네비게이션 */}
-      <NavBarWrapper>
-        <StyledTabs value={selectTab} onChange={handleChange} centered>
-          <Tab
-            style={activeStyle(selectTab === 0)}
-            label={<StyledLabel>스터디 모집</StyledLabel>}
-          />
-          <Tab
-            style={activeStyle(selectTab === 1)}
-            label={<StyledLabel>질문 게시판</StyledLabel>}
-          />
-          <Tab
-            style={activeStyle(selectTab === 2)}
-            label={<StyledLabel>자유 게시판</StyledLabel>}
-          />
-        </StyledTabs>
-      </NavBarWrapper>
-      {/* {selectTab === 0 && <RecruitBoardPage />}
-      {selectTab === 1 && <QuestionBoardPage />}
-      {selectTab === 2 && <FreeBoardPage />} */}
-    </div>
+    // 페이지 네이게이션
+    <NavBarWrapper>
+      <StyledTabs value={selectTab} onChange={handleChange} centered>
+        <Tab
+          style={activeStyle(selectTab === 0)}
+          label={<StyledLabel>스터디 모집</StyledLabel>}
+        />
+        <Tab
+          style={activeStyle(selectTab === 1)}
+          label={<StyledLabel>질문 게시판</StyledLabel>}
+        />
+        <Tab
+          style={activeStyle(selectTab === 2)}
+          label={<StyledLabel>자유 게시판</StyledLabel>}
+        />
+      </StyledTabs>
+    </NavBarWrapper>
   );
 }
 
