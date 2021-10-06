5c9f65812710b01fba548f1485f27dc39d93e8c0
seungwonleee <seungwon.code@gmail.com>
2021-10-07 00:25:48 +0900

    Add: recruit post form

--- client/src/pages/RecruitFormPage.tsx
+++ client/src/pages/RecruitFormPage.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import PostForm from '@components/PostForm';
-import { PageContainer1, FormContainer, PageTitle } from '@assets/styles/page';
+import RecruitPostForm from '@components/RecruitPostForm';
+import { PageContainer1, PageTitle, PageBackground } from '@assets/styles/page';
 
 function RecruitFormPage(): JSX.Element {
   return (
-    <PageContainer1>
-      <PageTitle>스터디 모집 글 작성</PageTitle>
-      <FormContainer width='100%' padding='3%'>
-        <PostForm />
-      </FormContainer>
-    </PageContainer1>
+    <PageBackground>
+      <PageContainer1>
+        <PageTitle>스터디 모집 글 작성</PageTitle>
+        <RecruitPostForm />
+      </PageContainer1>
+    </PageBackground>
   );
 }
 
