161a2d14b161e7599cdfe3f7da919ee92b9f0884
leeceo97 <leeceo97@naver.com>
2021-10-11 23:58:42 +0900

    (test)image

--- Server/profiles/views.py
+++ Server/profiles/views.py
@@ -197,7 +197,7 @@ class TeamProfileCreate(GenericAPIView):
         """
 
         serializer = TeamProfileSerializer(data=request.data)
-        print(request.body)
+        print(request.data['image'])
         if serializer.is_valid():
             serializer.save()
             profile = TeamProfile.objects.get(id=serializer.data['id'])
