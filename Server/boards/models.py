93dd548956cd37043b38300d8a7b47c0a6605b0e
leeceo97 <leeceo97@naver.com>
2021-10-29 18:48:04 +0900

    (fix)board title max_length

--- Server/boards/models.py
+++ Server/boards/models.py
@@ -20,7 +20,7 @@ class CommonBoard(models.Model):
 class RecruitmentBoard(models.Model):
 
     user = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, null=True)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=256)
     study = models.ForeignKey('profiles.TeamProfile', on_delete=models.CASCADE, null=False, blank=False)
     developer = models.PositiveIntegerField(default=0)
     designer = models.PositiveIntegerField(default=0)
