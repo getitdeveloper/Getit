6f8a6a2e5818c269b488481ade36e0f91e8dfe9a
seungwonleee <seungwon.code@gmail.com>
2021-12-02 18:36:55 +0900

    Fix: user authentication

--- client/src/auth/UserAuthentication/index.tsx
+++ client/src/auth/UserAuthentication/index.tsx
@@ -12,28 +12,28 @@ const auth = (
     const history = useHistory();
     const dispatch = useDispatch();
 
-    useLayoutEffect(() => {
-      if (option === true) {
-        axios
-          .get('/api/auth/')
-          .then((response) => {
-            console.log('서버와 쿠키 공유 상태 response ===> ', response);
-            dispatch({
-              type: USER_PROFILE_REQUEST,
-              data: {
-                user_pk: response.data.user_pk,
-              },
-            });
-          })
-          .catch((error) => {
-            console.log('서버와 쿠키 공유 상태 error ===>', error);
-            alert('로그인이 필요합니다. 로그인 후 이용해 주세요.');
-            return history.push('/');
-          });
-      } else if (option === false) {
-        return history.push('/');
-      }
-    }, []);
+    // useLayoutEffect(() => {
+    //   if (option === true) {
+    //     axios
+    //       .get('/api/auth/')
+    //       .then((response) => {
+    //         console.log('서버와 쿠키 공유 상태 response ===> ', response);
+    //         dispatch({
+    //           type: USER_PROFILE_REQUEST,
+    //           data: {
+    //             user_pk: response.data.user_pk,
+    //           },
+    //         });
+    //       })
+    //       .catch((error) => {
+    //         console.log('서버와 쿠키 공유 상태 error ===>', error);
+    //         alert('로그인이 필요합니다. 로그인 후 이용해 주세요.');
+    //         return history.push('/');
+    //       });
+    //   } else if (option === false) {
+    //     return history.push('/');
+    //   }
+    // }, []);
 
     return <SpecificComponent />;
   };
