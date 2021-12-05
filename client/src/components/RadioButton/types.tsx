f3ba46c7d2e03d881f986f9b3a30e3b646e72a9f
seungwonleee <seungwon.code@gmail.com>
2021-12-05 21:44:53 +0900

    Fix: job sort radio button, recruit post form code refactoring

--- client/src/components/RadioButton/types.tsx
+++ client/src/components/RadioButton/types.tsx
@@ -1,10 +1,12 @@
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, ChangeEvent } from 'react';
 
 export interface IRadioButton {
   item: {
     text: string;
     value: string;
     checked: boolean;
-  };
+    count?: number;
+  }[];
   onClick: MouseEventHandler<HTMLInputElement>;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
 }
--- client/src/components/SortButton/NewFamous.tsx
+++ client/src/components/SortButton/NewFamous.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useCallback } from 'react';
 import Popover from '@material-ui/core/Popover';
 import Typography from '@material-ui/core/Typography';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
@@ -6,25 +6,37 @@ import { SortButton, useStyles } from './styles';
 
 export default function NewFamous(): JSX.Element {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
-    null,
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const [selectTab, setSelectTab] = useState<string>('최신순');
+
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+    },
+    [anchorEl],
   );
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+  const handlePostSort = useCallback(
+    (event: any) => {
+      setSelectTab(event.target.innerHTML);
+      setAnchorEl(null);
+    },
+    [selectTab, anchorEl],
+  );
 
-  const handleClose = () => {
+  const handleClose = useCallback(() => {
     setAnchorEl(null);
-  };
+  }, [anchorEl]);
 
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
 
+  // TODO 백엔드 api 만들어지면 selectTab 값에 따른 요청
+
   return (
     <div>
       <SortButton aria-describedby={id} onClick={handleClick}>
-        <span>최신순</span>
+        <span>{selectTab}</span>
         <ArrowDropDownIcon />
       </SortButton>
       <Popover
@@ -41,8 +53,12 @@ export default function NewFamous(): JSX.Element {
           horizontal: 'center',
         }}
       >
-        <Typography className={classes.typography}>최신순</Typography>
-        <Typography className={classes.typography}>인기순</Typography>
+        <Typography className={classes.typography} onClick={handlePostSort}>
+          최신순
+        </Typography>
+        <Typography className={classes.typography} onClick={handlePostSort}>
+          인기순
+        </Typography>
       </Popover>
     </div>
   );
