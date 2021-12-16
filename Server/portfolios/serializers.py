57372cff8930cab3d8d3c55e269d304651e38317
leeceo97 <leeceo97@naver.com>
2021-12-16 18:15:10 +0900

    (fix)profile serializer add teamprofile, portfolio

--- Server/portfolios/serializers.py
+++ Server/portfolios/serializers.py
@@ -5,13 +5,7 @@ from .models import Portfolio
 
 
 class PortfolioSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Portfolio
         fields = ('id','user', 'title','link', 'contents', 'image',)
-
-    def get_image(self, obj):
-        request = self.context.get('request')
-        image = obj.image.url
-        return request.build_absolute_uri(image)
