47ce96aa706520d5b3078d2fea11abbf7e082a44
leeceo97 <leeceo97@naver.com>
2021-12-21 02:20:23 +0900

    (add)board filter, search

--- Server/boards/admin.py
+++ Server/boards/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from rest_framework.fields import ChoiceField
 
 # Register your models here.
-from .models import RecruitmentBoard, CommonBoard
+from .models import RecruitmentBoard, CommonBoard, Worker
 
-admin.site.register([CommonBoard, RecruitmentBoard])
+admin.site.register([CommonBoard, RecruitmentBoard, Worker])
